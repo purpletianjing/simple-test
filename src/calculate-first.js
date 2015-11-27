d6d5bfe66305ea882b9f4478ffbfc7bcec8d5bf4
purpletianjing <purpletianjing@163.com>
2015-11-27 16:00:24 +0800

    write the first programming to achieve th e first spec

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,7 +1,20 @@
+
 function single_element(arr) {
-  return arr.filter(function(val, key) {
-    return key % 2 === 1
-  }).filter(function(val, key, newArr) {
-    return newArr.indexOf(val) === newArr.lastIndexOf(val);
-  })
+	var filterResult = arr.filter(function(val,index){
+		return index % 2 !== 0;
+	});
+	var count = 0;
+	var result = [];
+	filterResult.forEach(function(val) {
+		count = 0;
+		for(var i = 0;i < filterResult.length;i++) {
+			if(val === filterResult[i]) {
+				count++;
+			}
+		}
+		if(count === 1) {
+			result.push(val);
+		}
+	});
+	return result;
 }
