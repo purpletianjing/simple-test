9f7fec6102d6b7c0bb3c82b3660b95235d95225b
purpletianjing <purpletianjing@163.com>
2015-11-27 16:48:51 +0800

    add the content to src/calculate-second.js

--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,21 +1,21 @@
-function median(arr) {
-  var newArr = arr.concat(); //复制一份，防止求中位数导致源数组改变
-  var len = newArr.length;
-  newArr.sort(function(a, b) {
+function median(filterArr) {
+  var result = 0;
+  var len = filterArr.length;
+  filterArr.sort(function(a,b) {
     return a - b;
   });
-
-  if(len % 2 == 0) {
-    return (newArr[len/2-1] + newArr[len/2]) / 2;
-  } else {
-    return newArr[(len-1)/2];
+  if(len % 2 !== 0) {
+    return filterArr[parseInt(len / 2)];
   }
+  return (filterArr[len / 2] + filterArr[len / 2 - 1]) / 2;
 }
 
 function calculate_median(arr) {
-  var list = arr.filter(function(val, key) {
-    return key % 2 === 1;
-  })
-
-  return median(list);
+  var filterArr = arr.filter(function(val,index) {
+    return (index + 1) % 2 === 0;
+  });
+   return median(filterArr);
 }
+
+
+
